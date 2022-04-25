b26b0cfdb69f927a5147957145f8b63970e28ac8
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2022-04-25 12:31:57 -0500

    Enabling CORS.

--- Chapter07/server/index.js
+++ Chapter07/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { createServer } = require('http');
 const { ApolloServer } = require('apollo-server-express');
 
@@ -6,6 +7,7 @@ const resolvers = require('./resolvers.js');
 const typeDefs = require('./typeDefs.js');
 
 const app = express();
+app.use(cors())
 
 const server = new ApolloServer({
   typeDefs,
